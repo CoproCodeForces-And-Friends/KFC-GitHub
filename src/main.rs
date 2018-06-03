e59327f17aaee989e140bb229e0e4e6d2ccadd35
Evgeny Fomin <fominok@hotmail.com>
2018-06-04 01:05:17 +0300

    TDD gh header

--- src/main.rs
+++ src/main.rs
@@ -3,71 +3,8 @@ extern crate hyper;
 extern crate failure;
 extern crate futures;
 
-use futures::future::{ok, err, result};
-use hyper::service::Service;
-use hyper::{Body, Request, Response, StatusCode};
-use tokio::runtime::current_thread::Runtime;
-
-use hyper::rt::{Future, run};
-use futures::Stream;
-
-struct Test;
-
-impl Test {
-    pub fn new() -> Self {
-        Test
-    }
-}
-
-impl Service for Test {
-    type ReqBody = Body;
-    type ResBody = Body;
-    type Error = hyper::Error;
-    type Future = Box<Future<Item = Response<Self::ResBody>, Error = Self::Error>>;
-    fn call(&mut self, req: Request<Self::ReqBody>) -> Self::Future {
-        let res = Response::builder()
-            .status(StatusCode::OK)
-            .body(Body::from("Hah haaay"))
-            .unwrap();
-
-        Box::new(ok(res))
-    }
-}
+mod webhook;
 
 fn main() {
-    println!("Lel");
-}
 
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    fn sts(s: hyper::Body) -> String {
-        let body = s.map_err(|_| ()).fold(vec![], |mut acc, chunk| {
-            acc.extend_from_slice(&chunk);
-            Ok(acc)
-        }).and_then(|v| String::from_utf8(v).map_err(|_| ()));
-        body.wait().unwrap()
-    }
-
-    #[test]
-    fn top_test() {
-        let mut server = Test::new();
-        let req = Request::builder()
-            .uri("http://fominok.ru")
-            .header("User-Agent", "my-agent/1.0")
-            .body(Body::from(""))
-            .unwrap();
-
-        let good = Response::builder()
-            .status(StatusCode::OK)
-            .body(Body::from("Hah haaay"))
-            .unwrap();
-
-
-        let mut rt = Runtime::new().unwrap();
-        let resp = server.call(req);
-        let r = rt.block_on(resp).unwrap();
-        assert_eq!(sts(good.into_body()), sts(r.into_body()));
-    }
 }
---
+++ src/webhook.rs
@@ -0,0 +1,124 @@
+extern crate tokio;
+extern crate hyper;
+extern crate failure;
+extern crate futures;
+
+use futures::future::ok;
+use futures::{Future,Stream};
+use tokio::runtime::current_thread::Runtime;
+
+use hyper::service::Service;
+use hyper::{Method, Body, Request, Response, StatusCode};
+
+struct WebhookService;
+
+impl WebhookService {
+    pub fn new() -> Self {
+        WebhookService
+    }
+}
+
+fn check_agent<T>(req: &Request<T>) -> bool {
+    req.headers().get("User-Agent")
+        .map_or(false, |hv| {
+            if let Ok(s) = hv.to_str() {
+                s.split("/").next()
+                    .map_or(false, |x| x == "GitHub-Hookshot")
+            } else {
+                false
+            }
+        })
+}
+
+impl Service for WebhookService {
+    type ReqBody = Body;
+    type ResBody = Body;
+    type Error = hyper::Error;
+    type Future = Box<Future<Item = Response<Self::ResBody>, Error = Self::Error>>;
+
+    fn call(&mut self, req: Request<Self::ReqBody>) -> Self::Future {
+        let mut res = Response::new(Body::empty());
+
+        match (req.method(), req.uri().path()) {
+            (&Method::POST, "/webhook") => {
+                if check_agent(&req) {
+
+                } else {
+                    *res.status_mut() = StatusCode::FORBIDDEN;
+                }
+
+            }
+            (&Method::GET, "/simple") => {
+                *res.body_mut() = Body::from("{}");
+            }
+            _ => {
+                *res.status_mut() = StatusCode::NOT_FOUND;
+            }
+        };
+        Box::new(ok(res))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn sts(s: hyper::Body) -> String {
+        let body = s.map_err(|_| ()).fold(vec![], |mut acc, chunk| {
+            acc.extend_from_slice(&chunk);
+            Ok(acc)
+        }).and_then(|v| String::from_utf8(v).map_err(|_| ()));
+        body.wait().unwrap()
+    }
+
+    fn process_req(service: &mut WebhookService, req: Request<Body>) -> Response<Body> {
+        let mut rt = Runtime::new().unwrap();
+        let resp = service.call(req);
+        rt.block_on(resp).unwrap()
+    }
+
+    #[test]
+    fn test_example() {
+        let req_good = Request::builder()
+            .uri("/simple")
+            .header("User-Agent", "GitHub-Hookshot/16abbec")
+            .header("Content-Type", "application/json")
+            .body(Body::from("{}"))
+            .unwrap();
+
+        let res_good = Response::builder()
+            .status(StatusCode::OK)
+            .body(Body::from("{}"))
+            .unwrap();
+
+        let mut service = WebhookService::new();
+        let r = process_req(&mut service, req_good);
+        assert_eq!(sts(res_good.into_body()), sts(r.into_body()));
+    }
+
+    #[test]
+    fn test_rejects_bad_user_agent() {
+        let req = Request::builder()
+            .uri("/webhook")
+            .method("POST")
+            .header("User-Agent", "cool/agent")
+            .header("Content-Type", "application/json")
+            .body(Body::from("{}"))
+            .unwrap();
+
+        let mut service = WebhookService::new();
+        let r = process_req(&mut service, req);
+        assert_eq!(r.status(), StatusCode::FORBIDDEN);
+
+        let req2 = Request::builder()
+            .uri("/webhook")
+            .method("POST")
+            .header("User-Agent", "GitHub-Hookshot/16abbec")
+            .header("Content-Type", "application/json")
+            .body(Body::from("{}"))
+            .unwrap();
+
+        let r2 = process_req(&mut service, req2);
+        assert_eq!(r2.status(), StatusCode::OK);
+    }
+}
